50df5d289ff287590de4672f96d587b6f9caa5e5
Bala Iyer <balaji.iyer@just-eat.com>
2018-08-02 08:54:00 +0100

    First pass of proxy to upstream service

---
+++ src/yakbak/YakbakMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
+
+namespace Yakbak
+{
+    public class YakbakMiddleware
+    {
+        private readonly YakbakOptions _options;
+        private readonly RequestDelegate _next;
+
+        public YakbakMiddleware(YakbakOptions options, RequestDelegate next)
+        {
+            _options = options;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = context.Request;
+            HydrateFromRequest(request);
+            var host = new HostString(_options.HostName);
+            var scheme = _options.Scheme;
+            var uri = new Uri(UriHelper.BuildAbsolute(scheme, host, request.PathBase, request.Path, request.QueryString));
+            await context.ProxyRequest(uri);
+        }
+
+        private void HydrateFromRequest(HttpRequest request)
+        {
+            if (!string.IsNullOrEmpty(request.Headers["Host"]))
+            {
+                _options.HostName = request.Headers["Host"];
+            }
+
+            if (!string.IsNullOrEmpty(request.Headers["scheme"]))
+            {
+                _options.Scheme = request.Headers["scheme"];
+            }
+            else
+            {
+                _options.Scheme = "http";
+            }
+        }
+    }
+
+    public class YakbakOptions
+    {
+        public string HostName { get; set; }
+        public string Scheme { get; set; } = "http";
+    }
+}
---
+++ src/yakbak/YakbakServerExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Yakbak
+{
+    public static class YakbakServerExtensions
+    {
+        public static HttpRequestMessage CreateProxyHttpRequest(this HttpContext context, Uri uri)
+        {
+            var request = context.Request;
+
+            var requestMessage = new HttpRequestMessage();
+            var requestMethod = request.Method;
+            if (!HttpMethods.IsGet(requestMethod) &&
+                !HttpMethods.IsHead(requestMethod) &&
+                !HttpMethods.IsDelete(requestMethod) &&
+                !HttpMethods.IsTrace(requestMethod))
+            {
+                var streamContent = new StreamContent(request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            // Copy the request headers
+            foreach (var header in request.Headers)
+            {
+                if (!requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray()) && 
+                    requestMessage.Content != null)
+                {
+                    requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+                }
+            }
+
+            requestMessage.Headers.Host = uri.Authority;
+            requestMessage.RequestUri = uri;
+            requestMessage.Method = new HttpMethod(request.Method);
+
+            return requestMessage;
+        }
+
+        public static async Task ProxyRequest(this HttpContext context, Uri destinationUri)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (destinationUri == null)
+            {
+                throw new ArgumentNullException(nameof(destinationUri));
+            }
+
+            using (var requestMessage = context.CreateProxyHttpRequest(destinationUri))
+            {
+                using (var responseMessage = await context.SendProxyHttpRequest(requestMessage))
+                {
+                    await context.CopyProxyHttpResponse(responseMessage);
+                }
+            }
+        }
+
+        public static Task<HttpResponseMessage> SendProxyHttpRequest(this HttpContext context, HttpRequestMessage requestMessage)
+        {
+            if (requestMessage == null)
+            {
+                throw new ArgumentNullException(nameof(requestMessage));
+            }
+
+            var client = new HttpClient(new HttpClientHandler {AllowAutoRedirect = false, UseCookies = false});
+            return client.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted);
+        }
+
+        public static async Task CopyProxyHttpResponse(this HttpContext context, HttpResponseMessage responseMessage)
+        {
+            if (responseMessage == null)
+            {
+                throw new ArgumentNullException(nameof(responseMessage));
+            }
+
+            var response = context.Response;
+
+            response.StatusCode = (int)responseMessage.StatusCode;
+            foreach (var header in responseMessage.Headers)
+            {
+                response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            foreach (var header in responseMessage.Content.Headers)
+            {
+                response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            // SendAsync removes chunking from the response. This removes the header so it doesn't expect a chunked response.
+            response.Headers.Remove("transfer-encoding");
+
+            using (var responseStream = await responseMessage.Content.ReadAsStreamAsync())
+            {
+                const int streamCopyBufferSize = 81920;
+                await responseStream.CopyToAsync(response.Body, streamCopyBufferSize, context.RequestAborted);
+            }
+        }
+
+        public static void UseYakbak(this IApplicationBuilder app, YakbakOptions options = null)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            app.UseMiddleware<YakbakMiddleware>(options ?? new YakbakOptions());
+        }
+    }
+}
